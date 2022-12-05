cc4b1d7c9e019e0ae3587b708e59fe1e9f1f3415
triaki-nabil-prog <nabil.triaki2@gmail.com>
2022-12-05 13:08:34 +0100

    pubsub testing sending data

--- app.js
+++ app.js
@@ -1,62 +1,80 @@
-//GameBoard module
-const GameBoard =(function(){
-    const GameBoard =[];
+// pubsub module
+let pubsub = {
+    subscriptions: {
+        GameData: []
+    },
+    subscribe: function (subscriptionName, fn) {
+        this.subscriptions[subscriptionName] = this.subscriptions[subscriptionName] || [];
+        this.subscriptions[subscriptionName].push(fn);
+    },
+    unsubscribe: function (subscriptionName, fn) {
+        if (this.subscriptions[subscriptionName]) {
+            const index = this.subscriptions.indexOf(fn);
+            if (index !== -1) this.subscriptions[subscriptionName].splice(index, 1);
+        }
+    },
+    publish: function (subscriptionName, data) {
+        if (this.subscriptions[subscriptionName]) {
+            this.subscriptions[subscriptionName].forEach(fn => { fn(data); });
+        }
+    }
+};
 
-    return{};
-}
 
-)();
+
+
+
 
 //Game flow control module
-const GameFlowControl =(function(){
-    
+let GameFlowControl = (function () {
+    pubsub.subscribe("GameData", render);
+    function render(data) {
+        console.log("render" + data); // getting game data from the gameboard
+    }
+})();
 
-    return{};
-}
 
-)();
+
+
+
+
 
 // control display module of the gameBoard on the DOM
-const DisplayController =(function(){
-    
+let DisplayController = (function () {
 
-    return{};
-}
+    pubsub.subscribe("GameData", render);
 
-)();
+    function render(data) {
+        console.log("render" + data); // getting game data from the gameboard
+    }
+
+
+})();
+
+
+
+
+
+//GameBoard module
+let GameBoard = (function () {
+    let GameBoardData = ["X", "O", "O", "X", "O", "O", "X", "O", "O"];
+    pubsub.publish("GameData", GameBoardData);
+
+
+})();
+
+
+
+
+
+
+
 
 //player creation 
-const player = function(name){
-
-
-    return{name,};
+let player = function (name) {
+    return { name, };
 }
 
 // a two player game
 PlayerOne = new player("player One");
 PlayerTwo = new player("player Tow");
-
-
-
-
-
-// pubsub module
-const pubsub = {
-	subscriptions: {
-	},
-	subscribe: function(subscriptionName, fn) {
-		this.subscriptions[subscriptionName] = this.subscriptions[subscriptionName] || [];
-		this.subscriptions[subscriptionName].push(fn);
-	},
-	unsubscribe: function(subscriptionName, fn) {
-		if (this.subscriptions[subscriptionName]) {
-			const index = this.subscriptions.indexOf(fn);
-			if (index !== -1) this.subscriptions[subscriptionName].splice(index, 1);
-		}
-	},
-	publish: function(subscriptionName, data) {
-		if (this.subscriptions[subscriptionName]) {
-			this.subscriptions[subscriptionName].forEach(fn => { fn(data); });
-		}
-	}
-};
